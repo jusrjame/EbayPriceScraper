acefec49edd17fd8995762fb01c81b421b4db47d
Justin James <jusrjame@ut.utm.edu>
2017-05-10 00:47:16 -0500

    extracts the first n number of prices on the ebay webpage and gives the average price of the item(s).  n is hardcoded at 5

--- priceScraper.py
+++ priceScraper.py
@@ -1,35 +1,48 @@
+
 import sys
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Auction=1&_nkw="
-sorting = "&_sop=1"
-
-if len(sys.argv) >= 1:
+if len(sys.argv) > 1:
 
     # list of command line arguments except for
-    # the program name itself
+    # the program itself
     arguments = sys.argv[1:]
 
-    for word in arguments:
+    for item in arguments:
+
+        url = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Auction=1&_nkw=" + item + "&_sop=1"
+
         # site holds an open version of the website
-        page = urllib2.urlopen(url + word + sorting)
+        page = urllib2.urlopen(url)
 
         soup = BeautifulSoup(page, "html.parser")
 
-        # print soup
-
-        # finds all <span> tags
+        # finds all span with class="bold" tags
         tags = soup.find_all('span', 'bold')
 
-        #iterate thorough all span tags and print out the
-        #the price data for each gpu on the page.
-        for tag in tags:
-            print tag.get_text()
+        prices_text = []
+        number_of_prices = 10
+        # get the first 5 prices on the webpage
+        for i in range(0, number_of_prices):
+            prices_text.append(tags[i].get_text())
 
-        # website holds a copy of the webpage contents
-        # website = site.read()
+        # drop the '$' and save prices as a list of floats
+        prices_float = []
+        for price in prices_text:
+            print price
+            p = price[1:].replace(',', '')
+            prices_float.append(float(p))
 
-        # f = open(word + ".html", "w")
-        # f.write(website)
-        # f.close
+        total = 0.0
+        # add the top 5 prices together
+        for price in prices_float:
+            total += price
+
+        # get the average price of the top 5
+        average_price = total / number_of_prices
+
+        print ("{}: {}".format(item, average_price))
+
+else:
+    print ("Enter an item")
